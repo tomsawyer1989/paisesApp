2fc10e4226d99865ebdbeda8553dc522a2f0feb5
Jorge Leonardo Herrera Ortiz <herrera.jorge@correounivalle.edu.co>
2021-07-18 20:55:14 -0500

    feat(RouterModule - Rutas en nuestra aplicación)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { SharedModule } from './shared/shared.module';
 import { PaisModule } from './pais/pais.module';
 
@@ -12,6 +14,7 @@ import { PaisModule } from './pais/pais.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SharedModule,
     PaisModule
   ],
