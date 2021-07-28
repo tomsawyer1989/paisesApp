6b24f9f72765636df357ca95baa82232b58ba160
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2021-07-27 22:26:28 -0500

    feat(Componente Sidebar)

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,6 +3,12 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-sidebar',
     templateUrl: 'sidebar.component.html',
-    styleUrls: ['sidebar.component.css']
+    styles: [
+        `
+            li {
+                cursor: pointer;
+            }
+        `
+    ]
 })
 export class SidebarComponent {}
