9847a7ac2372c7d52c09862ecd80ebb8ce8dbfdb
Jorge Leonardo Herrera Ortiz <leonardo.herrera@imexhs.com>
2021-08-03 00:32:27 -0500

    feat(Llenar la tabla de países)

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { PaisService } from "../../services/pais.service";
 
+import { Country } from "../../interfaces/pais.interface";
+
 @Component({
     selector: 'app-por-pais',
     templateUrl: 'por-pais.component.html'
@@ -8,6 +10,7 @@ import { PaisService } from "../../services/pais.service";
 export class PorPaisComponent {
     termino: string = '';
     hayError: boolean = false;
+    paises: Country[] = [];
 
     constructor (private paisService: PaisService) {}
 
@@ -16,9 +19,10 @@ export class PorPaisComponent {
 
         this.paisService.buscarPais(this.termino)
         .subscribe(response => {
-            console.log(response);
+            this.paises = response;
         }, err => {
             this.hayError = true;
+            this.paises = [];
         });
     }
 }
--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 import { FormsModule } from "@angular/forms";
 
 import { PorCapitalComponent } from "./pages/por-capital/por-capital.component";
@@ -22,6 +23,7 @@ import { VerPaisComponent } from "./pages/ver-pais/ver-pais.component";
     ],
     imports: [
         CommonModule,
+        RouterModule,
         FormsModule
     ],
 })
