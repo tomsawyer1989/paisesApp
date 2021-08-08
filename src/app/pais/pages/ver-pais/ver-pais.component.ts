d32c0c392357a797a870bb706a221965a927f7d7
Jorge Leonardo Herrera Ortiz <leonardo.herrera@imexhs.com>
2021-08-08 14:05:32 -0500

    feat(Servicio para Ver País)

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { PaisService } from "../../services/pais.service";
 
 @Component({
     selector: 'app-ver-pais',
     templateUrl: 'ver-pais.component.html',
 })
-export class VerPaisComponent {}
+export class VerPaisComponent implements OnInit {
+
+    constructor (
+        private activatedRoute: ActivatedRoute,
+        private paisService: PaisService
+    ) {}
+
+    ngOnInit (): void {
+        this.activatedRoute.params
+        .subscribe(({ id }) => {
+            this.paisService.getPaisPorAlpha(id)
+            .subscribe(response => {
+                console.log(response);
+            });
+        });
+    }
+}
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -23,4 +23,10 @@ export class PaisService {
 
         return this.http.get<Country[]>(url);
     }
+
+    getPaisPorAlpha (termino: string): Observable<Country> {
+        const url = `${ this.apiUrl }/alpha/${ termino }`;
+
+        return this.http.get<Country>(url);
+    }
 }
