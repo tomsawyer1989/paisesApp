546ae57ac505c465813acfc9e4b8baddbba7d35d
Jorge Leonardo Herrera Ortiz <leonardo.herrera@imexhs.com>
2021-08-08 21:03:57 -0500

    feat(Clase css condicionada)

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -3,5 +3,23 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-por-region',
     templateUrl: 'por-region.component.html',
+    styles: [
+        `
+            button {
+                margin-right: 5px;
+            }
+        `
+    ]
 })
-export class PorRegionComponent {}
+export class PorRegionComponent {
+    regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+    regionActiva: string = '';
+
+    getClaseCSS (region: string): string {
+        return this.regionActiva === region ? 'btn btn-primary' : 'btn btn-outline-primary';
+    }
+
+    activarRegion (region: string) {
+        this.regionActiva = region;
+    }
+}
