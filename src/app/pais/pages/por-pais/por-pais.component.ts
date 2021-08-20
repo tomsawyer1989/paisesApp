c7e4130405787ced40b06dc07c0b0a4beb646596
Jorge Leonardo Herrera Ortiz <leonardo.herrera@imexhs.com>
2021-08-20 00:13:43 -0500

    feat(Mostrar sugerencias al escribir -)

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,17 +5,27 @@ import { Country } from "../../interfaces/pais.interface";
 
 @Component({
     selector: 'app-por-pais',
-    templateUrl: 'por-pais.component.html'
+    templateUrl: 'por-pais.component.html',
+    styles: [
+        `
+            li {
+                cursor: pointer;
+            }
+        `
+    ]
 })
 export class PorPaisComponent {
     termino: string = '';
     hayError: boolean = false;
     paises: Country[] = [];
+    paisesSugeridos: Country[] = [];
+    mostrarSugerencias: boolean = false;
 
     constructor (private paisService: PaisService) {}
 
     buscar (termino: string) {
         this.hayError = false;
+        this.mostrarSugerencias = false;
         this.termino = termino;
 
         this.paisService.buscarPais(termino)
@@ -29,7 +39,18 @@ export class PorPaisComponent {
 
     sugerencias (termino: string) {
         this.hayError = false;
-        
-        // crear sugerencias
+        this.mostrarSugerencias = true;
+        this.termino = termino;
+
+        this.paisService.buscarPais(termino)
+        .subscribe(response => {
+            this.paisesSugeridos = response.splice(0, 3);
+        }, err => {
+            this.paisesSugeridos = [];
+        });
+    }
+
+    buscarSugerido (termino: string) {
+        this.buscar(termino);
     }
 }
